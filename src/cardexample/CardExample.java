1c2cefa4241b21e91e08f8754ee9039a1eab2801
ahmed <mirzahme@sheridancollege.ca>
2020-01-13 12:04:55 -0400

    meepo

---
+++ src/cardexample/CardExample.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardexample;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class CardExample {
+private String suit;
+private int value;
+
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
+}
